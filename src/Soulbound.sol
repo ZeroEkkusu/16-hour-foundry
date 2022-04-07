4a7238e5394de64c12cc5fbaa314abec0486380a
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2022-04-07 19:23:53 +0200

    feat: `Soulbound` mvp

---
+++ src/Soulbound.sol
@@ -0,0 +1,138 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+pragma solidity ^0.8.4;
+
+/// @notice Soulbound NFT contract based on Vitalik Buterin's
+/// @notice https://vitalik.ca/general/2022/01/26/soulbound.html
+/// @dev The Soulbound ERC standard does not exists yet!
+contract Soulbound {
+    error TokenIdCannotBeNonZero();
+    error InvalidForSoulbound();
+    error Unauthorized();
+    error EnsCallFailed();
+    error ResolverCallFailed();
+
+    event Updated(address _ensAddr);
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 indexed _tokenId
+    );
+    event Approval(
+        address indexed _owner,
+        address indexed _approved,
+        uint256 indexed _tokenId
+    );
+    event ApprovalForAll(
+        address indexed _owner,
+        address indexed _operator,
+        bool _approved
+    );
+
+    string public constant name = "Soulbound Pug";
+    string public constant symbol = "SBP";
+    string private constant URI =
+        "ipfs:QmfHdx2jYHhimPayeXrsnhSbby2FSBsieQhw5dYkbhhdpR";
+
+    bytes32 private immutable namehash;
+    address private ensAddr;
+
+    constructor(bytes32 _namehash, address _ensAddr) {
+        namehash = _namehash;
+        ensAddr = _ensAddr;
+    }
+
+    function tokenURI(uint256 _tokenId) external pure returns (string memory) {
+        if (_tokenId != 0) revert TokenIdCannotBeNonZero();
+        return URI;
+    }
+
+    function balanceOf(address _owner) external view returns (uint256) {
+        return _owner == resolveAddress() ? 1 : 0;
+    }
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        if (_tokenId != 0) revert TokenIdCannotBeNonZero();
+        return resolveAddress();
+    }
+
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId,
+        bytes memory data
+    ) external payable {
+        revert InvalidForSoulbound();
+    }
+
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable {
+        revert InvalidForSoulbound();
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable {
+        revert InvalidForSoulbound();
+    }
+
+    function approve(address _approved, uint256 _tokenId) external pure {
+        revert InvalidForSoulbound();
+    }
+
+    function setApprovalForAll(address _operator, bool _approved)
+        external
+        pure
+    {
+        revert InvalidForSoulbound();
+    }
+
+    function getApproved(uint256 _tokenId) external pure returns (address) {
+        revert InvalidForSoulbound();
+    }
+
+    function isApprovedForAll(address _owner, address _operator)
+        external
+        pure
+        returns (bool)
+    {
+        return false;
+    }
+
+    function supportsInterface(bytes4 interfaceID)
+        external
+        pure
+        returns (bool)
+    {
+        if (
+            interfaceID == 0x01ffc9a7 ||
+            interfaceID == 0x80ac58cd ||
+            interfaceID == 0x5b5e139f
+        ) return true;
+    }
+
+    function resolveAddress() private view returns (address) {
+        (bool success, bytes memory data) = ensAddr.staticcall(
+            abi.encodeWithSignature("resolver(bytes32)", namehash)
+        );
+        if (!success) revert EnsCallFailed();
+        address resolver = abi.decode(data, (address));
+        (success, data) = resolver.staticcall(
+            abi.encodeWithSignature("addr(bytes32)", namehash)
+        );
+        if (!success) revert ResolverCallFailed();
+        address owner = abi.decode(data, (address));
+        return owner;
+    }
+
+    function update(address _ensAddr) external {
+        if (msg.sender != resolveAddress()) revert Unauthorized();
+        ensAddr = _ensAddr;
+        emit Updated(_ensAddr);
+    }
+}
---
+++ src/test/Soulbound.t.sol
@@ -0,0 +1,148 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+pragma solidity ^0.8.4;
+
+import "src/Soulbound.sol";
+
+import {DSTest} from "ds-test/test.sol";
+import {Vm} from "forge-std/Vm.sol";
+
+contract SoulboundTest is DSTest {
+    event Updated(address _ensAddr);
+
+    address constant ENS_ADDR = 0x314159265dD8dbb310642f98f50C066173C1259b;
+    bytes32 constant NAMEHASH =
+        0xee6c4522aab0003e8d14cd40a6af439055fd2577951148c14b6cea9a53475835;
+    address constant OWNER_ADDRESS = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
+
+    Soulbound soulbound;
+
+    Vm vm = Vm(HEVM_ADDRESS);
+
+    function setUp() public {
+        soulbound = new Soulbound(NAMEHASH, ENS_ADDR);
+    }
+
+    function testName() public {
+        assertEq(soulbound.name(), "Soulbound Pug");
+    }
+
+    function testSymbol() public {
+        assertEq(soulbound.symbol(), "SBP");
+    }
+
+    function testTokenUriIdZero() public {
+        assertEq(
+            soulbound.tokenURI(0),
+            "ipfs:QmfHdx2jYHhimPayeXrsnhSbby2FSBsieQhw5dYkbhhdpR"
+        );
+    }
+
+    function testCannotGetTokenUriIdNonZero(uint256 tokenId) public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Soulbound.TokenIdCannotBeNonZero.selector)
+        );
+        vm.assume(tokenId != 0);
+        soulbound.tokenURI(tokenId);
+    }
+
+    function testBalanceOfOwner() public {
+        assertEq(soulbound.balanceOf(OWNER_ADDRESS), 1);
+    }
+
+    function testBalanceOfNonOwner(address addr) public {
+        vm.assume(addr != OWNER_ADDRESS);
+        assertEq(soulbound.balanceOf(addr), 0);
+    }
+
+    function testOwnerOfIdZero() public {
+        assertEq(soulbound.ownerOf(0), OWNER_ADDRESS);
+    }
+
+    function testCannotGetOwnerOfIdNonZero(uint256 tokenId) public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Soulbound.TokenIdCannotBeNonZero.selector)
+        );
+        vm.assume(tokenId != 0);
+        soulbound.ownerOf(tokenId);
+    }
+
+    function testCannotSafeTransferFromWithDataInvalid() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Soulbound.InvalidForSoulbound.selector)
+        );
+        soulbound.safeTransferFrom(address(0), address(0), 0, bytes(""));
+    }
+
+    function testCannotSafeTransferFromWithoutDataInvalid() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Soulbound.InvalidForSoulbound.selector)
+        );
+        soulbound.safeTransferFrom(address(0), address(0), 0);
+    }
+
+    function testCannotTransferFromInvalid() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Soulbound.InvalidForSoulbound.selector)
+        );
+        soulbound.safeTransferFrom(address(0), address(0), 0);
+    }
+
+    function testCannotApproveInvalid() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Soulbound.InvalidForSoulbound.selector)
+        );
+        soulbound.approve(address(0), 0);
+    }
+
+    function testCannotSetApprovalForAllInvalid() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Soulbound.InvalidForSoulbound.selector)
+        );
+        soulbound.setApprovalForAll(address(0), true);
+    }
+
+    function testCannotGetApprovedInvalid() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Soulbound.InvalidForSoulbound.selector)
+        );
+        soulbound.getApproved(0);
+    }
+
+    function testIsApprovedForAll() public {
+        assert(!soulbound.isApprovedForAll(address(0), address(0)));
+    }
+
+    function testSupportsInterface() public {
+        assert(soulbound.supportsInterface(0x01ffc9a7));
+        assert(soulbound.supportsInterface(0x80ac58cd));
+        assert(soulbound.supportsInterface(0x5b5e139f));
+    }
+
+    function testSupportsInterfaceRandom(bytes4 randomInterface) public {
+        vm.assume(randomInterface != 0x01ffc9a7);
+        vm.assume(randomInterface != 0x80ac58cd);
+        vm.assume(randomInterface != 0x5b5e139f);
+        assert(!soulbound.supportsInterface(randomInterface));
+    }
+
+    function testUpdate() public {
+        vm.prank(OWNER_ADDRESS);
+        address newEnsAddr = address(999);
+        vm.expectEmit(false, false, false, true);
+        emit Updated(newEnsAddr);
+        soulbound.update(newEnsAddr);
+        address loadedEnsAddr = address(
+            uint160(uint256(vm.load(address(soulbound), bytes32(uint256(0)))))
+        );
+        assertEq(loadedEnsAddr, newEnsAddr);
+    }
+
+    function testCannotUpdateUnauthorized() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Soulbound.Unauthorized.selector)
+        );
+        vm.prank(address(0xBAD));
+        soulbound.update(address(0));
+    }
+}
